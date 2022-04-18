b3122778e912c94acedcf0b2456008c4e39ca4cc
Samuelsntr <dodisamuel20@gmail.com>
2022-04-18 11:33:55 +0800

    adding other section, not good enough

--- lib/main.dart
+++ lib/main.dart
@@ -11,46 +11,210 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            toolbarHeight: 230, // default is 56
-            toolbarOpacity: 0.9,
-            title: Column(
-              children: [
-                Center(
-                  child: Container(
-                    child: CircleAvatar(
-                      radius: 50,
-                      backgroundImage: AssetImage('assets/images/me.jpg'),
-                    ),
+        appBar: AppBar(
+          toolbarHeight: 230, // default is 56
+          toolbarOpacity: 0.9,
+          title: Column(
+            children: [
+              Center(
+                child: Container(
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage('assets/images/me.jpg'),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(9.0),
-                  child: Text(
-                    "Samuel Sianturi",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(9.0),
+                child: Text(
+                  "Samuel Sianturi",
+                  style: TextStyle(fontWeight: FontWeight.bold),
                 ),
-                Text("Student", style: TextStyle(height: 1, fontSize: 14))
-              ],
-            ),
+              ),
+              Text("Student", style: TextStyle(height: 1, fontSize: 14))
+            ],
           ),
-          body: Column(
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Text(
-                      "About me",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(15),
+          child: Column(
+            children: [
+              Text("About me", style: TextStyle(fontWeight: FontWeight.bold)),
+              Container(
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(color: Colors.black, width: 0.2))),
+                  child: Text(
+                      "Hi, currently I am studying at the Fakulty of Enginerring and Informatics at the Universitas Pendidikan Nasional. My point of interest includes web development, mobile apps development, and UI/UX",
+                      maxLines: 4,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.black.withOpacity(0.5)))),
+              Container(
+                margin: EdgeInsets.all(10),
+                child: Text("Skills",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ),
+              Container(
+                  decoration: BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(color: Colors.black, width: 0.2))),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                "5",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,
+                              ),
+                              Icon(
+                                Icons.star,
+                                color: Colors.orange,
+                                size: 18.0,
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text("HTML",
+                                  style: TextStyle(
+                                      color: Colors.black.withOpacity(0.5)))
+                            ],
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                "4",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,
+                              ),
+                              Icon(
+                                Icons.star,
+                                color: Colors.orange,
+                                size: 18.0,
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text("CSS",
+                                  style: TextStyle(
+                                      color: Colors.black.withOpacity(0.5)))
+                            ],
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                "3",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,
+                              ),
+                              Icon(
+                                Icons.star,
+                                color: Colors.orange,
+                                size: 18.0,
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text("JavaScript",
+                                  style: TextStyle(
+                                      color: Colors.black.withOpacity(0.5)))
+                            ],
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                "3",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,
+                              ),
+                              Icon(
+                                Icons.star,
+                                color: Colors.orange,
+                                size: 18.0,
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text("Flutter",
+                                  style: TextStyle(
+                                      color: Colors.black.withOpacity(0.5)))
+                            ],
+                          )
+                        ],
+                      ),
+                    ],
+                  )),
+              Container(
+                margin: EdgeInsets.all(20),
+                child: Column(children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Find me on",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
                   ),
-                ],
+                  Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Icon(
+                              Icons.facebook,
+                              color: Colors.blueAccent,
+                              size: 18.0,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Icon(
+                              Icons.whatsapp,
+                              color: Colors.blueAccent,
+                              size: 18.0,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Icon(
+                              Icons.telegram,
+                              color: Colors.blueAccent,
+                              size: 18.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  )
+                ]),
               )
             ],
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
